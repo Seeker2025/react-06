9a077ec816bd908018165109aaab684e02c52390
Igor Yurovsky <85400723+Seeker2025@users.noreply.github.com>
2026-04-13 10:20:37 +0300

    go react

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,63 @@
-import {Counter} from '../Counter/Counter';
-import {Dropdown} from '../Dropdown/Dropdown';
+import React, { Component } from 'react';
 
-export const App = () => {
-  return (
-    <div>
-      <h1>React homework template</h1>
-      {/* <Counter initialValue={10}/> */}
+import { Counter       }     from '../Counter/Counter';
+import { Dropdown      }     from '../Dropdown/Dropdown';
+import { ColorPicker   }     from '../ColorPicker/ColorPicker';
+import   TodoList            from 'TodoList/TodoList';
+import   initialTodos        from 'todos';
 
-      <Counter />
 
-      <Dropdown/>
-    
-    </div>
+const colorPickerOptions = [
+    {label: 'red',    color: '#F44336'},
+    {label: 'green',  color: '#4CAF50'},
+    {label: 'blue',   color: '#2196F3'},
+    {label: 'grey',   color: '#607D8B'},
+    {label: 'pink',   color: '#E91E63'},
+    {label: 'indigo', color: '#3F51B5'},
+
+];
+
+class App extends Component {
+  state = {
+    todos: initialTodos,
+  };
+
+  deleteTodo = todoId =>{
+    this.setState(prevState =>({
+      todos: prevState.todos.filter(todo=>todo.id!==todoId),
+    }))
+  }
+    render(){
+      const{ todos } = this.state;
+
+      const completedTodos = todos.reduce(
+        (acc, todo)=>(todo.completed ? acc + 1 : acc),
+        0,
+      )
+       return (
+            <>
+             
+              {/* <Counter initialValue={10}/> */}
+
+              <Counter />
+
+              <Dropdown/>
+
+              <ColorPicker options ={colorPickerOptions}/>
+
+              <div>
+                <p>Общее кол-во: {todos.length}</p>
+                <p>Кол-во выполненных: {completedTodos}</p>
+              </div>
+
+              <TodoList 
+              todos={this.state.todos}
+              onDeleteTodo={this.deleteTodo}
+              />
+            
+            </>
   );
-};
+ }
+}
+
+export { App };
